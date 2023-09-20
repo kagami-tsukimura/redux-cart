0c317d603b4e0cdb0557c2b481e097cadd3ec2c8
Kagami <shimura.kyohei7617@gmail.com>
2023-09-20 22:12:08 +0900

    style: define article informations

---
+++ src/cartItems.js
@@ -0,0 +1,32 @@
+const cartItems = [
+  {
+    id: 1,
+    title: '貫禄ある皇帝ペンギンと従者たち',
+    price: '18000',
+    img: '../images/1_emperor.jpg',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: '埋もれるジェンツーペンギン',
+    price: '14000',
+    img: '../images/2_gentoo.jpg',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: '優雅に泳ぐフンボルトペンギン',
+    price: '9800',
+    img: '../images/3_humboldt.jpg',
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: '愛くるしいフェアリーペンギン',
+    price: '49800',
+    img: '../images/4_fairy.jpg',
+    amount: 1,
+  },
+];
+
+export default cartItems;
