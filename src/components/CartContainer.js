9c97e5ed327a383710a90eed0841152ebf216d87
Kagami <shimura.kyohei7617@gmail.com>
2023-09-21 21:00:27 +0900

    style: empty cart template

---
+++ src/components/CartContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const CartContainer = () => {
+  const { amount } = useSelector((store) => store.cart);
+  if (amount < 1) {
+    return (
+      <section className='cart'>
+        <header>
+          <h2>買い物かご</h2>
+          <h4 className='empty-cart'>何も入っていません・・・🐧</h4>
+        </header>
+      </section>
+    );
+  }
+};
+
+export default CartContainer;
